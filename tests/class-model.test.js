ce27c4bac7afe28dd3801dabfb03a93bd27a229b
Fernando Fragoso <fernandofragoso@gmail.com>
2017-07-28 17:51:16 -0300

    Remove test

--- tests/class-model.test.js
+++
@@ -1,6 +0,0 @@
-import ClassModel from '../src/class-model'
-
-test('ClassModel.functionModel', () => {
-  const testObject = new ClassModel('Test1');
-  expect(testObject.functionModel()).toBe("Function return for Test1 class");
-})
